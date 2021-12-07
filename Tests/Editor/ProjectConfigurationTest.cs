21315ed95cc2cd6ef658436185279aa2a232b5d0
mohsinkhan26 <mohsinkhan.se@gmail.com>
2021-12-07 12:56:37 +0800

    project configurations made separate from firebase + boundary image added

--- Tests/Editor/ProjectConfigurationTest.cs
+++ Tests/Editor/ProjectConfigurationTest.cs
@@ -1,10 +1,11 @@
-﻿using NUnit.Framework;
-/* 
+﻿/* 
  * Author : Mohsin Khan
  * Portfolio : http://mohsinkhan26.github.io/ 
  * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
  * Github : https://github.com/mohsinkhan26/
 */
+
+using NUnit.Framework;
 using UnityEngine;
 using MK.Common.Helpers;
 using MK.Common.Extensions;
@@ -29,31 +30,6 @@ namespace MK.AssetData
                         Application.platform.ToString().CaseSensitiveEquals(RuntimePlatform.WindowsEditor.ToString()) ||
                         Application.platform.ToString().CaseSensitiveEquals(RuntimePlatform.OSXEditor.ToString()),
                 "Current runtime platform is not supported");
-        }
-
-        [Test]
-        public void ProjectConfiguration_FirebaseProjectID_IsNotEmpty()
-        {
-            Assert.IsNotEmpty(projectConfiguration.FirebaseProjectID, "Firebase ProjectID field is empty");
-        }
-
-        [Test]
-        public void ProjectConfiguration_FirebaseEditorServiceAccountEmail_IsNotEmpty()
-        {
-            Assert.IsNotEmpty(projectConfiguration.FirebaseEditorServiceAccountEmail,
-                "Firebase EditorServiceAccountEmail field is empty");
-        }
-
-        [Test]
-        public void ProjectConfiguration_FirebaseEditorP12FileName_IsNotEmpty()
-        {
-            Assert.IsNotEmpty(projectConfiguration.FirebaseEditorP12FileName, "Firebase P12 FileName field is empty");
-        }
-
-        [Test]
-        public void ProjectConfiguration_FirebaseEditorP12Password_IsNotEmpty()
-        {
-            Assert.IsNotEmpty(projectConfiguration.FirebaseEditorP12Password, "Firebase P12 Password field is empty");
         }
 
         [Test]
