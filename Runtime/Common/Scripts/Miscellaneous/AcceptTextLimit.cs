2e7170a2faa565b968a7a1315bff8c5bb614657f
mohsinkhan26 <mohsinkhan.se@gmail.com>
2024-05-13 11:54:21 +0800

    acceptText Limit  + Show app version

---
+++ Runtime/Common/Scripts/Miscellaneous/AcceptTextLimit.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Author : Mohsin Khan
+ * Portfolio : http://mohsinkhan26.github.io/
+ * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
+ * Github : https://github.com/mohsinkhan26/
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace MK.Common.Miscellaneous
+{
+    [RequireComponent(typeof(TMP_InputField))]
+    public sealed class AcceptTextLimit : MonoBehaviour
+    {
+        [Tooltip("Accept more than this number, condition is greater than")]
+        public int minimumTextLimit = 0;
+
+        public TMP_InputField inputField;
+        public bool setInStart = true;
+        private ColorBlock colorBlock;
+        private ColorBlock colorBlockTemp = new ColorBlock();
+        public Color minimumLimitNotReached = new Color(0.933f, 0.325f, 0.313f); // light red color
+
+        private void Start()
+        {
+            if (inputField == null)
+                inputField = GetComponent<TMP_InputField>();
+            colorBlock = inputField.colors;
+            colorBlockTemp = inputField.colors;
+            colorBlockTemp.normalColor = minimumLimitNotReached;
+            colorBlockTemp.highlightedColor = minimumLimitNotReached;
+            colorBlockTemp.pressedColor = minimumLimitNotReached;
+            colorBlockTemp.selectedColor = minimumLimitNotReached;
+            if (setInStart) OnTextValueChange();
+        }
+
+        public void OnTextValueChange()
+        {
+            inputField.colors =
+                inputField.text.Length > minimumTextLimit ? colorBlock : colorBlockTemp;
+        }
+    }
+}
---
+++ Runtime/Common/Scripts/Miscellaneous/ShowAppVersion.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Author : Mohsin Khan
+ * Portfolio : http://mohsinkhan26.github.io/
+ * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
+ * Github : https://github.com/mohsinkhan26/
+ */
+
+using UnityEngine;
+using TMPro;
+using MK.Common.Extensions;
+
+namespace MK.Common.Miscellaneous
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public sealed class ShowAppVersion : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text versionText;
+
+        private void Awake()
+        {
+            Reset();
+        }
+
+        void OnEnable()
+        {
+            versionText.text = this.ConcatenateString("v",
+                Application.version);
+        }
+
+        public void HideVersion()
+        {
+            versionText.transform.parent.gameObject.SetActive(false);
+        }
+
+        // runs only in editor automatically, when you apply this script
+        private void Reset()
+        {
+            if (versionText == null)
+                versionText = GetComponent<TMP_Text>();
+        }
+    }
+}
