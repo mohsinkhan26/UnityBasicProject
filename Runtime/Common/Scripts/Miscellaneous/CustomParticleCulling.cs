617e1600137d7066b02d187f9aed183463d538da
mohsinkhan26 <mohsinkhan.se@gmail.com>
2024-05-13 11:57:38 +0800

    custom particle system added

---
+++ Runtime/Common/Scripts/Miscellaneous/CustomParticleCulling.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Author : Mohsin Khan
+ * Portfolio : http://mohsinkhan26.github.io/
+ * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
+ * Github : https://github.com/mohsinkhan26/
+ */
+
+using UnityEngine;
+
+// Reference: https://blog.unity.com/engine-platform/particlesystem-performance-culling-tips
+namespace MK.Common.Miscellaneous
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public sealed class CustomParticleCulling : MonoBehaviour
+    {
+        public float cullingRadius = 10;
+        public ParticleSystem target;
+
+        CullingGroup m_CullingGroup;
+        Renderer[] m_ParticleRenderers;
+
+        void OnEnable()
+        {
+            if (target == null)
+                target = GetComponent<ParticleSystem>();
+            if (m_ParticleRenderers == null)
+                m_ParticleRenderers = target.GetComponentsInChildren<Renderer>();
+
+            if (m_CullingGroup == null)
+            {
+                m_CullingGroup = new CullingGroup();
+                m_CullingGroup.targetCamera = Camera.main;
+                m_CullingGroup.SetBoundingSpheres(new[] {new BoundingSphere(transform.position, cullingRadius)});
+                m_CullingGroup.SetBoundingSphereCount(1);
+                m_CullingGroup.onStateChanged += OnStateChanged;
+
+                // We need to start in a culled state
+                Cull(m_CullingGroup.IsVisible(0));
+            }
+
+            m_CullingGroup.enabled = true;
+        }
+
+        void OnDisable()
+        {
+            if (m_CullingGroup != null)
+                m_CullingGroup.enabled = false;
+
+            target.Play(true);
+            SetRenderers(true);
+        }
+
+        void OnDestroy()
+        {
+            if (m_CullingGroup != null)
+                m_CullingGroup.Dispose();
+        }
+
+        void OnStateChanged(CullingGroupEvent sphere)
+        {
+            Cull(sphere.isVisible);
+        }
+
+        void Cull(bool visible)
+        {
+            if (visible)
+            {
+                // We could simulate forward a little here to hide that the system was not updated off-screen.
+                target.Play(true);
+                SetRenderers(true);
+            }
+            else
+            {
+                target.Pause(true);
+                SetRenderers(false);
+            }
+        }
+
+        void SetRenderers(bool enable)
+        {
+            // We also need to disable the renderer to prevent drawing the particles, such as when occlusion occurs.
+            foreach (var particleRenderer in m_ParticleRenderers)
+            {
+                particleRenderer.enabled = enable;
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (enabled)
+            {
+                // Draw gizmos to show the culling sphere.
+                Color col = Color.green;
+                if (m_CullingGroup != null && !m_CullingGroup.IsVisible(0))
+                    col = Color.gray;
+
+                Gizmos.color = col;
+                Gizmos.DrawWireSphere(transform.position, cullingRadius);
+            }
+        }
+    }
+}
---
+++ Runtime/Common/Scripts/Miscellaneous/CustomParticleSystem.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * Author : Mohsin Khan
+ * Portfolio : http://mohsinkhan26.github.io/
+ * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
+ * Github : https://github.com/mohsinkhan26/
+ */
+
+using UnityEngine;
+
+namespace MK.Common.Miscellaneous
+{
+    /// keep the references of all particle systems
+    [RequireComponent(typeof(ParticleSystem), typeof(CustomParticleCulling))]
+    public sealed class CustomParticleSystem : MonoBehaviour
+    {
+        public ParticleSystem[] particleSystems;
+        public ParticleSystemRenderer[] particleSystemRenders;
+        public CustomParticleCulling particleCulling;
+        private GameObject gObject;
+        public float StopAfter { get; private set; }
+
+        private bool IsParticlesPlaying
+        {
+            get
+            {
+                if (particleSystems == null) return false;
+                return particleSystems[0].isPlaying;
+            }
+        }
+
+        private void Awake()
+        {
+            if (gObject == null) gObject = gameObject;
+            Reset();
+        }
+
+#if UNITY_EDITOR
+        public
+#endif
+            // runs only in editor automatically, when you apply this script
+            void Reset()
+        {
+            if (particleSystems == null)
+            {
+                particleSystems = GetComponentsInChildren<ParticleSystem>();
+                particleSystemRenders = GetComponentsInChildren<ParticleSystemRenderer>();
+                particleCulling = GetComponent<CustomParticleCulling>();
+                particleCulling.target = particleSystems[0];
+            }
+        }
+
+        /// true: play,     false: stop
+        public void PlayParticles(bool _play)
+        {
+            for (int i = particleSystems.Length - 1; i >= 0; --i)
+            {
+                if (_play)
+                {
+                    particleSystems[i].Play();
+                    continue;
+                }
+
+                particleSystems[i].Stop();
+            }
+
+            // it's important to enable/disable the particle, otherwise custom culling script may turn it on/off
+            gObject.SetActive(_play);
+        }
+
+        public void StopParticlesAfter(float _after)
+        {
+            StopAfter = _after;
+            CancelInvoke(nameof(StopParticles));
+            Invoke(nameof(StopParticles), _after);
+        }
+
+        private void StopParticles()
+        {
+            PlayParticles(false);
+        }
+
+        public void SetStartColor(Color _color, int _indexParticle = 0)
+        {
+            var mainModule = particleSystems[_indexParticle].main;
+            mainModule.startColor = _color;
+        }
+
+        public void SetStartColorNested(Color _color)
+        {
+            for (int i = particleSystems.Length - 1; i >= 0; --i)
+            {
+                SetStartColor(_color, i);
+            }
+        }
+
+        public void SetShapeRadius(float _radius)
+        {
+            for (int i = particleSystems.Length - 1; i >= 0; --i)
+            {
+                var shape = particleSystems[i].shape;
+                shape.radius = _radius;
+            }
+
+            particleCulling.cullingRadius = _radius;
+        }
+
+        public void SetShapeRadiusMultiplier(float _radiusMultiplier)
+        {
+            for (int i = particleSystems.Length - 1; i >= 0; --i)
+            {
+                var shape = particleSystems[i].shape;
+                shape.radius *= _radiusMultiplier;
+            }
+        }
+
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            if (enabled && !IsParticlesPlaying) // if particles are not in playing mode, draw sphere on them
+            {
+                // Draw gizmos to show the not playing.
+                Color col = Color.red;
+
+                Gizmos.color = col;
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+            }
+        }
+#endif
+    }
+}
