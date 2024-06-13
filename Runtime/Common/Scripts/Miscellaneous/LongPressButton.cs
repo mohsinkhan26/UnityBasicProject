0518d9658afe4c55f5d4084ac54ca9bc7f95f31f
mohsinkhan26 <mohsinkhan.se@gmail.com>
2024-06-13 16:26:54 +0800

    long press button for 3 different scenarios

--- Runtime/Common/Scripts/Miscellaneous/LongPressButton.cs
+++ Runtime/Common/Scripts/Miscellaneous/LongPressButton.cs
@@ -11,29 +11,52 @@ using UnityEngine.EventSystems;
 
 namespace MK.Common.Miscellaneous
 {
-    [RequireComponent(typeof(EventTrigger))]
-    public class LongPressButton : MonoBehaviour
+    // NOTE: tested on uGUI
+    public class LongPressButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
+        private bool isPointerDown = false;
         private float startTime = 0f;
         [SerializeField] private float longPressTime = 5f;
-
-        /// Attach function which you want to call
         [SerializeField] private UnityEvent onLongPress;
+        [SerializeField] private UnityEvent<float> onLongPressValue; // returns time of press
+        [SerializeField] private UnityEvent onLongPressContinue; // function will be called multiple times or once
+        [SerializeField] private bool pointerDownContinueCallOnce = true;
+        private bool pointerDownContinueCallOnceTemp = true;
 
-        /// Add under PointerDown in EventTrigger,
         /// simply to get the elapsed time for this phase
-        public void OnPointerDown()
+        public void OnPointerDown(PointerEventData eventData)
         {
             startTime = Time.realtimeSinceStartup;
+            isPointerDown = true;
+            pointerDownContinueCallOnceTemp = true;
         }
 
-        /// Add under PointerUp in EventTrigger,
         /// Calculate and display the elapsed time
-        public void OnPointerUp()
+        public void OnPointerUp(PointerEventData eventData)
         {
+            isPointerDown = false;
             float elapsedTime = Time.realtimeSinceStartup - startTime;
-            if (elapsedTime >= longPressTime) onLongPress?.Invoke();
-            // this.Log("OnPointerUp-longPress elapsedTime: " + elapsedTime + "   LongTime: " + longPressTime);
+            if (elapsedTime < longPressTime) return;
+            onLongPress?.Invoke();
+            onLongPressValue?.Invoke(elapsedTime);
+            // this.Log("OnPointerUp-elapsedTime: " + elapsedTime + "   LongTime: " + longPressTime);
+        }
+
+        /// Calculate and display the elapsed time, function will be called multiple times
+        private void Update()
+        {
+            if (!isPointerDown) return; // don't run if pointer is not down already
+            if (!pointerDownContinueCallOnceTemp) return; // function is set to be called once only
+            float elapsedTime = Time.realtimeSinceStartup - startTime;
+            if (elapsedTime < longPressTime) return;
+            onLongPressContinue?.Invoke();
+            if (pointerDownContinueCallOnce && pointerDownContinueCallOnceTemp)
+            {
+                pointerDownContinueCallOnceTemp = false;
+            }
+
+            // this.Log("Update-elapsedTime: "
+            //          + elapsedTime + "   LongTime: " + longPressTime + "   Temp: " + pointerDownContinueCallOnceTemp);
         }
     }
 }
